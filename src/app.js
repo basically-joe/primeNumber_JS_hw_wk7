7e5a815a32f6abc64e31b6c740075282b2e43862
Joe McColl <joe4kool@gmail.com>
2019-01-14 20:39:12 +0000

    added logic top prime_number.js and required in app.js, working in npm run build and browser

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const PrimeChecker = require("./models/prime_number.js");
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 });
---
+++ src/models/prime_number.js
@@ -0,0 +1,19 @@
+const PubSub = require("../helpers/pub_sub.js");
+
+const PrimeChecker = function() {
+
+};
+
+PrimeChecker.prototype.numberIsPrime = function (number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        return false;
+    }
+  }
+  return true;
+};
+
+module.exports = PrimeChecker;
